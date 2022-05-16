0cef398d440471c290aa8bc7d4ce36efad267000
F-Jean <jean.courtot@gmail.com>
2022-05-16 11:56:39 +0200

    fix PR 'Functional tests #19' delete alice bundle qui vidait la bdd + ajout de /api à cause du firewall

--- tests/AbstractTest.php
+++ tests/AbstractTest.php
@@ -3,42 +3,37 @@
 namespace App\Tests;
 
 use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
-use Hautelook\AliceBundle\PhpUnit\RefreshDatabaseTrait;
+use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\Client;
 
 abstract class AbstractTest extends ApiTestCase
 {
-    private $token;
-
-    // This trait provided by AliceBundle will take care of refreshing the database content to a known state before each test
-    use RefreshDatabaseTrait;
-
-    public function setUp(): void
-    {
-        self::bootKernel();
-    }
+    private ?string $token = null;
 
     protected function createClientWithCredentials($token = null): Client
     {
         $token = $token ?: $this->getToken();
 
-        return static::createClient([], ['headers' => ['authorization' => 'Bearer '.$token]]);
+        return static::createClient([], ['headers' => ['authorization' => 'Bearer ' . $token]]);
     }
 
     protected function getToken($body = []): string
     {
         if ($this->token) {
+
             return $this->token;
         }
 
-        $response = static::createClient()->request('POST', '/authentication_token', ['body' => $body ?: [
-            'email' => '1@b2b.com',
-            'password' => 'password',
-        ]]);
+        $response = static::createClient()->request('POST', '/api/authentication_token', [
+            'json' => $body ?: [
+                'email' => '1@b2b.com',
+                'password' => 'password',
+            ]
+        ]);
 
         $this->assertResponseIsSuccessful();
         $data = json_decode($response->getContent());
-        $this->token = $data->access_token;
+        $this->token = $data->token;
 
-        return $data->access_token;
+        return $data->token;
     }
 }
