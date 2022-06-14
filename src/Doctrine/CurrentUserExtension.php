3a9ef572b48190810358160b8d1e12a4ca0aecfb
F-Jean <jean.courtot@gmail.com>
2022-06-14 16:32:44 +0200

    mod method in CurrentUserExtension : simplify & improve the test

--- src/Doctrine/CurrentUserExtension.php
+++ src/Doctrine/CurrentUserExtension.php
@@ -37,16 +37,14 @@ final class CurrentUserExtension implements QueryCollectionExtensionInterface, Q
 
     private function addWhere(QueryBuilder $queryBuilder, string $resourceClass): void
     {
-        // on vérifie que la ressource demandé est bien de type User || on met en place une exception si l'user à ROLE_ADMIN || on récupère l'user connecté au niveau du composant security
-        if (User::class !== $resourceClass || $this->security->isGranted('ROLE_ADMIN') || null === $user = $this->security->getUser()) {
-            return;
+        if ($resourceClass === User::class) {
+            // on récupère le premier alias racine
+            $alias = $queryBuilder->getRootAliases()[0];
+            // on peut alors utiliser cet alias pour la requête
+            $queryBuilder
+                ->andWhere("$alias.client = :current_user")
+                // on passe ensuite le paramètre ici current_user est l'id de l'user
+                ->setParameter('current_user', $this->security->getUser());
         }
-
-        // on récupère le premier alias racine
-        $rootAlias = $queryBuilder->getRootAliases()[0];
-        // on peut alors utiliser cet alias pour la requête
-        $queryBuilder->andWhere(sprintf('%s.client = :current_user', $rootAlias));
-        // on passe ensuite le paramètre ici current_user est l'id de l'user
-        $queryBuilder->setParameter('current_user', $user->getId());
     }
 }
